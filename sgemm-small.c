269960a24c01fe3e0abf39cc4410448ef1fa1bb3
Yew_Hung_Leong <cs61c-mq@hive23.CS.Berkeley.EDU>
2012-10-20 15:53:06 -0700

    Working on mm

--- sgemm-small.c
+++ sgemm-small.c
@@ -1,37 +1,52 @@
 #include <stdio.h>
-#include <emmintrin.h>
+#include <nmmintrin.h>
 
 void sgemm( int m, int n, float *A, float *C )
 {
     /*Our code here*/
+    float *use_matrix = A;      //matrix to multiply: default *A
     
-    //Padding case only if m or n % 4 not equal 0:
-    float *padded_matrix;
+    //makes new padded dimensions
+    int new_m = (m + (4 - (m % 4)));
+    int new_n = (n + (4 - (n % 4)));
+    /*Padding case only if m or n % 4 not equal 0.
+    Might be slow.*/
     
-    if(m % 4 != 0 || n % 4 != 0)
+    if(m % 4 != 0 || n % 4 != 0)    //enters only if either matrix dimension is not a factor of 4
     {
-        int new_m = (m + (4 - (m % 4)));
-        int new_n = (n + (4 - (n % 4)));
-        int old_count = 0;
+        int old_count = 0;  //an iterator for the old matrix
         
-        padded_matrix = (float*) malloc(new_m * new_n * sizeof(float));
+        use_matrix = (float*) malloc(new_m * new_n * sizeof(float)); //allocates heap for padded matrix
                 
         for (int i = 0; i < new_m; i++){
             for (int j = 0; j < new_n; j++){
                 if (i > n){
-                    padded_matrix[i] = 0.0;
+                    use_matrix[i] = 0.0;
                 } else if (j > m){
-                    padded_matrix[i] = 0.0;
+                    use_matrix[i] = 0.0;
                 } else{ 
-                    padded_matrix[i] = A[old_count];
+                    use_matrix[i] = A[old_count];
                     old_count++;
                 }
             }
         }
     }
     
-    for (int i = 0; i < n; i = i + 4){
-        __m128 c_matrix = _mm_loadu_ps(C + i);
-        
+    /*Multiplying with padding to account for fringe cases*/
+    for (int i = 0; i < n; i++){ //outer loop
+        for (int j = 0; j < new_m; j++){
+            __m128 a1 = _mm_loadu_ps(A + 0);
+            //__m128 a2 = _mm_loadu_ps(A + 4);
+            //__m128 a1 = _mm_loadu_ps(A + 0);
+            for (int k = 0; k < new_m; k++){
+                __m128 a_transpose1 = _mm_loadu_ps(A + (i + k * new_n));
+                //__m128 a2 = _mm_loadu_ps(
+                
+                //Loading C Matrix
+                __128 c1 = _mm_loadu_ps(C + 4);
+                
+                
+            }
+        }
     }
 }
